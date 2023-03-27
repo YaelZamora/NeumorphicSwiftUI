2db8801adf1cc55af1a953f116d4232cdfca72f2
YaelZamora <ajedrezkasparov@gmail.com>
2023-03-27 15:20:00 -0600

    Initial Commit

---
+++ neumorphic/neumorphicApp.swift
@@ -0,0 +1,17 @@
+//
+//  neumorphicApp.swift
+//  neumorphic
+//
+//  Created by Yael Javier Zamora Moreno on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct neumorphicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
